22181ef4b9071651c8e569f5adcd28c2f6a1ea25
Bahiyyah <70109610+bahiyyah-4@users.noreply.github.com>
2021-01-28 14:20:20 +0300

    Delete server directory

--- server/config/project.config.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-mongoose.connect("mongodb://localhost/ProjectManager", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-})
-    .then(() => console.log("Established a connection to the database"))
-    .catch(err => console.log("Something went wrong when connecting to the database", err));
